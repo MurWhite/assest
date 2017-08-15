7b198d96842355ee7255e5d2e751b8e756d911e0
白波 <baibo@meituan.com>
2017-08-15 10:09:20 +0800

    初始化sass编辑脚本

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+let gulp = require('gulp');
+let pump = require('pump');
+let del = require('del');
+let sass = require('gulp-sass');
+let distDir = './docs/',
+    sassSrc = './raw/**/*.sass', sassDist = `${distDir}css`;
+
+gulp.task('default', ['clean', 'sass'], () => {
+  console.log('编译完成')
+});
+
+gulp.task('sass', _ => {
+  pump([
+    gulp.src(sassSrc),
+    sass.sync({outputStyle: 'compressed'}).on('error', sass.logError),
+    gulp.dest(sassDist)
+  ], _);
+});
+gulp.task('sass:clean', _ => del(sassDist));
+gulp.task('sass:watch', _ => {
+  gulp.watch(sassSrc, ['sass']);
+});
+
+
+gulp.task('clean', _ => del(distDir));
+
+gulp.task('watch', ['sass:watch'])
