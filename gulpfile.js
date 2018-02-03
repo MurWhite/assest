2b202fc81b00d68f0dc345ebbd4527ba1d3f9db3
白波 <baibo@meituan.com>
2018-02-03 20:56:25 +0800

    update wiki.css

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ let gulp = require('gulp');
 let pump = require('pump');
 let del = require('del');
 let sass = require('gulp-sass');
-let distDir = './docs/',
-    sassSrc = './raw/*.sass', sassDist = `${distDir}css`;
+let distDir = './docs',
+    sassSrc = './raw/*.sass', sassDist = `${distDir}/css`;
 
 gulp.task('default', ['clean', 'sass'], () => {
   console.log('编译完成')
@@ -24,4 +24,4 @@ gulp.task('sass:watch', _ => {
 
 gulp.task('clean', _ => del(distDir));
 
-gulp.task('watch', ['sass:watch'])
+gulp.task('watch', ['sass:watch']);
